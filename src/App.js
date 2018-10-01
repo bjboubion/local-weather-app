30e939c1a0aa5747445c159eff8c2ac9c2533597
bjboubion <bjboubion@gmail.com>
2018-10-01 12:37:26 -0700

    break

--- src/App.js
+++ src/App.js
@@ -1,18 +1,49 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Axios from "axios";
+import "./App.css";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      weatherData: {
+        coord: {},
+        weather: [{}],
+        main: {}
+      }
+    };
+  }
+
+  componentDidMount() {
+    Axios.get(
+      "https://fcc-weather-api.glitch.me/api/current?lat=33.92119&lon=-117.864357"
+    ).then(res => {
+      const weatherData = res.data;
+      this.setState({ weatherData });
+      console.log(weatherData);
+    });
+  }
+
+  celsiusToFahrenheit() {}
+
+  fahrenheitToCelsius() {}
+
   render() {
+    const { weatherData } = this.state;
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <header className="App-header">Local Weather App</header>
+        <main>
+          <div className="App-body">
+            <h3>You are located in: {weatherData.name}</h3>
+            <h3>Temperature: {weatherData.main.temp}</h3>
+            <h4>Min: {weatherData.main.temp_min}</h4>
+            <h4>Max: {weatherData.main.temp_max}</h4>
+            <h3>{weatherData.weather[0].description}</h3>
+            <img src={weatherData.weather[0].icon} alt="weather image" />
+            {/* <h2>{navigator.geolocation.getCurrentPosition()}</h2> */}
+          </div>
+        </main>
       </div>
     );
   }
